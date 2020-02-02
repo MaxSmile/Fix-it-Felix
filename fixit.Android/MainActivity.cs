69d4b2025939bc0ec6a6ff0e901581572f7be616
Developer Vasilkoff <dev.vasilkoff@gmail.com>
2020-02-02 12:18:50 +0200

    changed android icons

--- fixit.Android/MainActivity.cs
+++ fixit.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace fixit.Droid
 {
-    [Activity(Label = "fixit", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "fixit", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
